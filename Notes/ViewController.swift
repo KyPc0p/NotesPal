adf97765e7d04389c39aa0cafccb327c6c42f88a
Artem Kharchenko <artem.kh4rchenko@ya.ru>
2023-01-18 19:47:18 +0700

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Артём Харченко on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
